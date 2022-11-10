e78cc5d29e670e1da4dd0db637a77dafe2e978d8
Şule <sule.kaptan@bil.omu.edu.tr>
2022-11-10 23:17:25 +0300

    build the basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import "@fontsource/press-start-2p";
 
 function App() {
-  
+  return(
+    <div>
+        <h1>How fast do you type?</h1>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <h4>Time Remaining: ???</h4>
+        <button>Start</button>
+        <h1>Word count: ???</h1>
+    </div>
+  )
 }
 
 export default App
